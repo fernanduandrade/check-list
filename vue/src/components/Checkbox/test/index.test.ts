b19c9c10e994be4eeee2c46dbc80ba38b9a4da30
fernanduandrade <onanduandrade@gmail.com>
2023-05-24 06:29:12 -0300

    tests: adicionado mais casos de testes para component CheckBox

--- vue/src/components/Checkbox/test/index.test.ts
+++ vue/src/components/Checkbox/test/index.test.ts
@@ -30,4 +30,47 @@ describe('Casos de teste para o component checkbox', () => {
      it('Component deve ter a prop priority com state oragen', () => {
         expect(wrapper.props('priority')).toBe('orange')
      })
+
+     it('Component deve ter a prop modelValue com state false após o click', async () => {
+      const checkboxWrapper = mount(CheckboxVue, ({ props: { 
+         disabled: false,
+         modelValue: true,
+         priority: 'yellow'
+      }}))
+
+      const input = checkboxWrapper.find('input')
+      await input.trigger('click')
+      await checkboxWrapper.vm.$nextTick()
+      expect(checkboxWrapper.emitted('update:modelValue')).toBeTruthy()
+   })
+
+   it('Componente deve emitir o event com o valor false', async () => {
+      const wrapperCheckbox = mount(CheckboxVue, {
+        props: {
+         disabled: false,
+         modelValue: true,
+         priority: 'yellow'
+        },
+      });
+      
+      await wrapperCheckbox.find('input[type="checkbox"]').trigger('click');
+      const emittedvalue = wrapperCheckbox.emitted('update:modelValue')?.[0][0]
+      
+      expect(emittedvalue).toBe(false);
+    });
+
+    it('Componente não deve emitir o event se a prop disabled for true', async () => {
+      const wrapperCheckbox = mount(CheckboxVue, {
+        props: {
+         disabled: true,
+         modelValue: false,
+         priority: 'yellow'
+        },
+      });
+      
+      await wrapperCheckbox.find('input[type="checkbox"]').trigger('click');
+      const emittedvalue = wrapperCheckbox.emitted('update:modelValue')?.[0][0]
+      
+      expect(emittedvalue).toBe(undefined);
+    });
 })
